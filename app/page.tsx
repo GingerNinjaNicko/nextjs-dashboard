b6ea1a05a10d56a893c831405d1c746da931978a
Nick Ruddock <Nick.Ruddock@taxassist.co.uk>
2023-10-31 17:17:21 +0000

    💄 C3 - Adding fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import styles from './ui/home.module.css';
-
 import Link from 'next/link';
+
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
